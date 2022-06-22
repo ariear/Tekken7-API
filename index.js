d969047a1d5427b3611aa3e1ea0794f3de46d828
arie <arieinjector@gmail.com>
2022-06-22 23:05:32 +0700

    fixed static image

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import 'dotenv/config'
+import helmet from 'helmet';
 import character from './src/router/character.js';
 import main from './src/router/index.js';
 
 const app = express()
+app.use(helmet())
 app.use('/asset',express.static('asset'))
 
 app.use(main)
