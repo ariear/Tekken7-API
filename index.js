e5430c2c1dc0da08a4aabd27a18db99e190e9853
arie <arieinjector@gmail.com>
2022-06-23 18:07:40 +0700

    fixed static image

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose';
 import 'dotenv/config'
 import cors from 'cors'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import character from './src/router/character.js';
 import main from './src/router/index.js';
 
 const app = express()
-app.use(express.static('public'))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use('/public', express.static(path.join(__dirname,'./public')))
 app.use(cors())
 
 app.use(main)
