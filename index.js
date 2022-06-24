9c068ef30e05bcb6b590b97770ba3a4c3e2c8e32
arie <arieinjector@gmail.com>
2022-06-24 18:26:00 +0700

    add prefix api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 app.use(cors())
 
 app.use(main)
-app.use(character)
+app.use('/api/v1/',character)
 
 mongoose.connect(process.env.APP_DB).then(() => {
     app.listen(3000, () => {
