afba034e4d5a78ecb93f3ac9888f076c5f284a33
arie <arieinjector@gmail.com>
2022-06-24 18:04:59 +0700

    fixed

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import 'dotenv/config'
 import cors from 'cors'
 import character from './src/router/character.js';
 import main from './src/router/index.js';
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url';
 
 const app = express()
-app.use(express.static('public'))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use('/public',express.static(path.join(__dirname,'public')))
 app.use(cors())
 
 app.use(main)
